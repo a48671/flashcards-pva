21d1bf37a9945c586557dcccc495025fd4ff2c70
Andrey Fomin <andreyfomin@Andreys-MacBook-Air.local>
2025-04-12 22:36:23 +0400

    the basename has been added

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,11 @@ import HomePage from '@/pages/home-page';
 import LearnPage from '@/pages/learn-page';
 import RepeatingPage from '@/pages/repeating-page';
 import { Navigation } from './presentation/components/navigation';
+import { BASENAME } from './config';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={ BASENAME }>
       <div className="min-h-screen flex flex-col">
         <main className="flex-grow px-4 pt-6 pb-24"> {/* pb-24 makes space for nav */}
           <Routes>
